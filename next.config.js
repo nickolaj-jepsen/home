3b7c43384c8866248bdd337e8c73755d02c43ade
Nickolaj Jepsen <nij@billigvvs.dk>
2023-08-16 01:28:41 +0200

    chore: enable strict mode

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withExportImages = require('next-export-optimize-images')
 
 const nextConfig = withExportImages({
     output: 'export',
+    reactStrictMode: true,
 })
 
 module.exports = nextConfig
